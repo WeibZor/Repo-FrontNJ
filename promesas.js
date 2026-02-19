3892aac2edd556b7be9359e91db2367a8b578819
WeibZor <ivesmolag@gmail.com>
2026-02-19 18:50:54 -0500

    Se realiza cambio de rama para poder ejecutar trabajos de clases principalmente para programación asíncrona

---
+++ promesas.js
@@ -0,0 +1,22 @@
+let peliculas = [
+    {
+    "titulo": "Megalodon",
+    "sinopsis": "Un tiburon que se come a la gente en una playa",
+    "genero": "Acción, Aventura",
+    "imagen": ""
+    }
+
+    {
+    "titulo": "La llorona",
+    "sinopsis": "Una mujer que perdio sus hijos",
+    "genero": "Teror, Mieod",
+    "imagen": "",
+    }
+    
+    {
+    "titulo": "El Padrino",
+    "sinopsis": "El capo de la mafia Italiana",
+    "genero": "Acción, Drama",
+    "imagen": "",
+    }
+]
