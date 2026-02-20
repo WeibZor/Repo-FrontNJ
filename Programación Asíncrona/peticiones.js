b4005373205bfe82ed7a52b71911ba91430bee8a
WeibZor <ivesmolag@gmail.com>
2026-02-19 20:13:46 -0500

    Se realiza creación de código línea 10 - 24 en la cual se realiza un fetch para tomar datos de una url externa, por medio de JSON placeholder user

--- Programación Asíncrona/peticiones.js
+++ Programación Asíncrona/peticiones.js
@@ -7,5 +7,18 @@ boton = document.querySelector("button");
 // Eventos
 
 boton.addEventListener("click", () =>{
-    alert("Todo bien");
+    // alert("Todo bien");
+    peticionApi();
 })
+
+// Función para realizar petición.
+
+function peticionApi(){
+    let urlExterna = "https://jsonplaceholder.typicode.com/users";
+    fetch(urlExterna)
+    .then((d) => d.json())
+    .then((datos) => {
+        console.log(datos);
+    })
+    .catch((error) => console.log(error));
+}
