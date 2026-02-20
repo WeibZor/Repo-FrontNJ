49ce779e4e0faa29051d03180ea74dbcb2312b3e
WeibZor <ivesmolag@gmail.com>
2026-02-19 19:22:58 -0500

    feat se raliza creaciṕn de códigos respesto creación de promesas con condicionales y adicionalmente a ello explicación de promesas

--- Programación Asíncrona/promesas.js
+++ Programación Asíncrona/promesas.js
@@ -22,8 +22,25 @@ let peliculas = [
 ]
 
 function ObtenerPeliculas(pelis) {
-    return pelis
+    // Simular retraso de 2 seg.
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+        // Validar si hay datos en la BD
+        if (pelis.length == 0){
+            reject("No se han encontrado películas")
+        } else{
+            resolve(pelis)
+        }
+        }, 2000)
+    })
+
+    // Esto es básicamnete para indicar a la consola que le espere 2000 milisegundos para mostrar los datos, ya que el proceso de obtener las peliculas es asincrono y tarda 2 segundos en completarse.
+    // Ahora bien la Promise puede brindar dos respuestas dado positiva como negativa resolve o reject, en este caso se esta utilizando resolve para indicar que la promesa se ha cumplido y se han obtenido las peliculas, pero si por alguna razon no se pudieran obtener las peliculas se podria utilizar reject para indicar que la promesa no se ha cumplido y se ha producido un error.
 }
 
 // mostrar datos
-console.log (ObtenerPeliculas(peliculas))
+ObtenerPeliculas(peliculas)
+.then((d) => console.log(d))
+.catch((e) => console.log(e))
+
+
